d34afeb0eb34567c058d223db9044e5567c08aed
p-hofmann <peter.hofmann@hhu.de>
2015-07-15 10:39:58 +0200

    inherit methods from GenomePreparation

--- communitydesign.py
+++ communitydesign.py
@@ -4,12 +4,11 @@ __author__ = 'hofmann'
 import os
 import random
 import tempfile
-from Bio import SeqIO
 from scripts.MetaDataTable.metadatatable import MetadataTable
-from scripts.Validator.sequencevalidator import SequenceValidator
 from scripts.StrainSimulationWrapper.strainsimulationwrapper import StrainSimulationWrapper
 from scripts.StrainSelector.strainselector import StrainSelector
 from scripts.PopulationDistribution.populationdistribution import PopulationDistribution
+from scripts.GenomePreparation.genomepreparation import GenomePreparation
 
 
 # ##################################
@@ -82,187 +81,12 @@ class Community(object):
 
 # ##################################
 #
-#          PrepareStrains
-#
-# ##################################
-
-
-class PrepareStrains(SequenceValidator):
-	_label = "PrepareStrains"
-
-	def _get_genome_id_to_path_map(self, file_path_of_file_mapping_genome_id_to_paths, list_of_drawn_genome_id):
-		"""
-		Get a dictionary mapping genome id to the path of their genome
-
-		@param file_path_of_file_mapping_genome_id_to_paths: File path to file with format 'id \t path'
-		@type file_path_of_file_mapping_genome_id_to_paths: str | unicode
-		@param list_of_drawn_genome_id: List of genome identifiers
-		@type list_of_drawn_genome_id: list[str|unicode]
-
-		@return: genome ids mapped to their gnome file path
-		@rtype: dict[str|unicode, str|unicode]
-		"""
-		mdt = MetadataTable(logfile=self._logfile, verbose=self._verbose)
-		mdt.read(file_path_of_file_mapping_genome_id_to_paths)
-		genome_id_to_path_map = mdt.get_map(0, 1, unique_key=True)
-		assert set(genome_id_to_path_map.keys()).issuperset(list_of_drawn_genome_id)
-		return {genome_id: genome_id_to_path_map[genome_id] for genome_id in list_of_drawn_genome_id}
-
-	def _move_genome_file(
-		self, file_path_input, file_path_output, sequence_min_length, set_of_sequence_names=None, file_format="fasta"):
-		"""
-		Move genomes into project folder, cleaning it up in the process.
-		Makes sure sequence ids are unique and descriptions/comments are removed
-		Returns total length of all sequences
-
-		@attention file_format: Anything but 'fasta' is not supported, yet
-
-		@param file_path_input: File path to raw genome
-		@type file_path_input: str | unicode
-		@param file_path_output: Destination path
-		@type file_path_output: str | unicode
-		@param sequence_min_length: Minimum length of sequences
-		@type sequence_min_length: int | long
-		@param set_of_sequence_names: Set of all previously used sequence names, making sure all will be unique
-		@type set_of_sequence_names: set[str|unicode]
-		@param file_format: 'fasta' format by default.
-		@type file_format: str | unicode
-
-		@return: Total length of all sequences
-		@rtype: int | long
-		@raise Exception:
-		"""
-		assert self.validate_file(file_path_input)
-		assert not self.validate_file(file_path_output, silent=True), "Overwriting files prohibited: '{}'".format(
-			file_path_output)
-		assert file_format == "fasta", "'{}' is not supported, yet.".format(file_format)
-		if set_of_sequence_names is None:
-			set_of_sequence_names = []
-		with open(file_path_input, 'r') as stream_input, open(file_path_output, 'w') as stream_output:
-			total_base_pairs = self._cleanup_and_filter_sequences(
-				stream_input, stream_output, sequence_min_length, set_of_sequence_names, file_format)
-		if total_base_pairs == 0:
-			msg = "No valid sequences in '{}'".format(stream_input.name)
-			self._logger.error(msg)
-			raise Exception(msg)
-		return total_base_pairs
-
-	def _cleanup_and_filter_sequences(
-		self, stream_input, stream_output, sequence_min_length, set_of_sequence_names, file_format="fasta"):
-		"""
-
-		@attention file_format: Anything but 'fasta' is not supported, yet
-
-		@param stream_input: input stream of sequence file
-		@type stream_input: file | FileIO | StringIO
-		@param stream_output: Output stream
-		@type stream_output: file | FileIO | StringIO
-		@param sequence_min_length: Minimum length of sequences
-		@type sequence_min_length: int | long
-		@param set_of_sequence_names: Set of all previously used sequence names, making sure all will be unique
-		@type set_of_sequence_names: set[str|unicode]
-		@param file_format: 'fasta' format by default.
-		@type file_format: str | unicode
-
-		@return: Total length of all sequences (base pairs)
-		@rtype: int | long
-		"""
-		total_base_pairs = 0
-		for seq_record in SeqIO.parse(stream_input, file_format):
-			# remove description, else art illumina messes up sam format
-			seq_record.description = ''
-			if len(seq_record.seq) < sequence_min_length:
-				self._logger.debug("'{}', Removing short sequence '{}', length: {}".format(
-					os.path.basename(stream_input.name), seq_record.id, len(seq_record.seq)))
-				continue
-			if seq_record.id in set_of_sequence_names:
-				seq_record.id = self._get_new_name(seq_record.id, set_of_sequence_names)
-			set_of_sequence_names.add(seq_record.id)
-			# file_handler.write(">{}\n".format(sequence_id))
-			# file_handler.writelines("{}\n".format(seq_record.seq))
-			stream_output.write(seq_record.format(file_format))
-			total_base_pairs += len(seq_record.seq)
-		return total_base_pairs
-
-	def move_genome_files(
-		self, genome_id_to_path_map, directory_output, sequence_min_length, set_of_sequence_names=None):
-		"""
-		Move and clean up a list of genomes
-
-		@param genome_id_to_path_map: Dictionary with file path by genome ids
-		@type genome_id_to_path_map: dict[str|unicode, str|unicode]
-		@param directory_output: Output directory
-		@type directory_output: str | unicode
-		@param sequence_min_length: Minimum length of sequences
-		@type sequence_min_length: int | long
-		@param set_of_sequence_names: Set of all previously used sequence names, making sure all will be unique
-		@type set_of_sequence_names: set[str|unicode]
-
-		@return:
-		@rtype: dict[str|unicode, int|long]
-		"""
-		assert isinstance(genome_id_to_path_map, dict)
-		if set_of_sequence_names is None:
-			set_of_sequence_names = set()
-		genome_id_to_total_length = {}
-		for genome_id, genome_file_path in genome_id_to_path_map.iteritems():
-			file_name = os.path.basename(genome_file_path)
-			new_genome_file_path = os.path.join(directory_output, file_name)
-			total_length = self._move_genome_file(
-				genome_file_path, new_genome_file_path, sequence_min_length, set_of_sequence_names)
-			genome_id_to_total_length[genome_id] = total_length
-		return genome_id_to_total_length
-
-	@staticmethod
-	def _get_new_name(name, set_of_sequence_names):
-		"""
-		Get a unique sequence name
-
-		@param name: Current sequence name
-		@type name: str | unicode
-		@param set_of_sequence_names: Set of all previously used sequence names, making sure all will be unique
-		@type set_of_sequence_names: set[str|unicode]
-		@return: Unique sequence name
-		@rtype : str | unicode
-		"""
-		index = 0
-		new_name = name
-		while new_name in set_of_sequence_names:
-			new_name = name + "_{}".format(index)
-			index += 1
-		return new_name
-
-	def validate_format(self, list_of_file_paths, file_format="fasta", sequence_type="dna", ambiguous=True):
-		"""
-		Validate file format of a list of fasta files
-
-		@param list_of_file_paths: List of fasta file paths
-		@type list_of_file_paths: list[str|unicode]
-		@param file_format: 'fasta' or 'fastq'
-		@type file_format: str | unicode
-		@param sequence_type: 'dna' or 'rna' or 'protein'
-		@type sequence_type: str | unicode
-		@param ambiguous: If true ambiguous characters are valid
-		@type ambiguous: bool
-
-		@return: True if all valid
-		@rtype: bool
-		"""
-		result = True
-		for file_path in list_of_file_paths:
-			if not self.validate_sequence_file(file_path, file_format, sequence_type, ambiguous):
-				result = False
-		return result
-
-
-# ##################################
-#
 #          CommunityDesign
 #
 # ##################################
 
 
-class CommunityDesign(PrepareStrains):
+class CommunityDesign(GenomePreparation):
 	"""
 		For the design of an artificial community
 	"""
@@ -320,20 +144,14 @@ class CommunityDesign(PrepareStrains):
 		self._tmp_dir = tmp_dir
 		assert self.validate_dir(self._tmp_dir)
 
-	def _write_distribution_files(
-		self, directory_output, genome_id_to_total_length, genome_id_to_abundance, genome_id_to_file_name,
-		community_id):
+	def _write_distribution_files(self, directory_output, genome_id_to_abundance, community_id):
 		"""
 		Write abundance file for each sample
 
 		@param directory_output: Distribution files will be stored here
 		@type directory_output: str | unicode
-		@param genome_id_to_total_length: Total sequence length of each genome
-		@type genome_id_to_total_length: dict[str|unicode, int|long]
 		@param genome_id_to_abundance: Drawn distribution for each genome id
 		@type genome_id_to_abundance: dict[str|unicode, list[float]]
-		@param genome_id_to_file_name: genome file path of each genome id
-		@type genome_id_to_file_name: dict[str|unicode, str|unicode]
 		@param community_id: Id of a community
 		@type community_id: int | str | unicode
 		"""
@@ -346,18 +164,13 @@ class CommunityDesign(PrepareStrains):
 			with open(out_file_path, 'w') as out_file_handler:
 				for genome_id in genome_id_to_abundance:
 					distribution = genome_id_to_abundance[genome_id][index_sample]
-					file_name = genome_id_to_file_name[genome_id]
-					total_length = genome_id_to_total_length[genome_id]
-					out_file_handler.write("{id}\t{file_name}\t{dist}\t{len}\n".format(
+					out_file_handler.write("{id}\t{distr}\n".format(
 						id=genome_id,
-						file_name=file_name,
-						dist=distribution,
-						len=total_length))
+						distr=distribution))
 
 	def design(
 		self, community, number_of_samples, metadata_table,
-		directory_out_distributions, directory_out_genomes, directory_out_metadata, directory_in_template=None,
-		set_of_sequence_names=None, min_sequence_length=1):
+		directory_out_distributions, directory_out_metadata, directory_in_template=None):
 		"""
 		Design artificial communities, of a specific design, for a given number of samples
 
@@ -369,25 +182,16 @@ class CommunityDesign(PrepareStrains):
 		@type metadata_table: MetadataTable
 		@param directory_out_distributions: Output directory where distribution files will be placed
 		@type directory_out_distributions: str | unicode
-		@param directory_out_genomes:  Output directory where cleaned up raw fasta files will be placed
-		@type directory_out_genomes: str | unicode
 		@param directory_out_metadata: Metadata tables of separated by chosen and not chosen genomes are written to here
 		@type directory_out_metadata: str | unicode
 		@param directory_in_template: contains template data for strain simulation
 		@type directory_in_template: str | unicode
-		@param set_of_sequence_names: Set of all previously used sequence names, making sure all will be unique
-		@type set_of_sequence_names: set[str|unicode]
-		@param min_sequence_length: Minimum sequence length
-		@type min_sequence_length: int | long
 
 		@return: List of drawn genome ids
 		@rtype: list[str|unicode]
 		"""
 		assert isinstance(community, Community)
 		assert isinstance(metadata_table, MetadataTable)
-
-		if set_of_sequence_names is None:
-			set_of_sequence_names = set()
 
 		number_of_strains = community.genomes_total
 
@@ -499,25 +303,25 @@ class CommunityDesign(PrepareStrains):
 		)
 
 		# move and clean up files (removes sequence description)
-		genome_id_to_total_length = self.move_genome_files(
-			genome_id_to_path_map,
-			directory_output=directory_out_genomes,
-			sequence_min_length=min_sequence_length,
-			set_of_sequence_names=set_of_sequence_names)
+		# genome_id_to_total_length = self.move_genome_files(
+		# 	genome_id_to_path_map,
+		# 	directory_output=directory_out_genomes,
+		# 	sequence_min_length=min_sequence_length,
+		# 	set_of_sequence_names=set_of_sequence_names)
 
 		# write distribution file
 		# genome_id_to_distributions = self._get_genome_id_to_distributions(list_of_drawn_genome_id, list_of_distributions)
 		assert len(list_of_drawn_genome_id) == len(list_of_distributions)
 		genome_id_to_distributions = dict(zip(list_of_drawn_genome_id, list_of_distributions))
 
-		genome_id_to_file_name = self._get_genome_id_to_file_name(genome_id_to_path_map)
+		# genome_id_to_file_name = self._get_genome_id_to_file_name(genome_id_to_path_map)
 		self._write_distribution_files(
 			directory_output=directory_out_distributions,
-			genome_id_to_total_length=genome_id_to_total_length,
+			# genome_id_to_total_length=genome_id_to_total_length,
 			genome_id_to_abundance=genome_id_to_distributions,
-			genome_id_to_file_name=genome_id_to_file_name,
+			# genome_id_to_file_name=genome_id_to_file_name,
 			community_id=community.id)
-		return list_of_drawn_genome_id
+		return genome_id_to_path_map
 
 	@staticmethod
 	def _get_genome_id_to_file_name(genome_id_to_path_map):
